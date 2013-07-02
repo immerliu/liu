e17b4ff26da2866959c1c7e838095906f98f45e3
immerliu <immerliu@gmail.com>
2013-07-01 22:23:50 -0700

    Update web.js

--- web.js
+++ web.js
@@ -1,3 +1,12 @@
+
+
+<head>
+<script type="text/javascript">
+function disp_alert()
+{
+alert("You are the best!");
+}
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
@@ -10,3 +19,10 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+</script>
+</head>
+<body>
+
+<input type="button" onclick="disp_alert()" value="Who is the best"  />
+
+</body>
