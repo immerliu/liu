0d2a8aab2db27b1474fd92e9a4a0a165d94da0dc
immerliu <immerliu@gmail.com>
2013-07-01 22:42:13 -0700

    Update web.js

--- web.js
+++ web.js
@@ -1,28 +1,27 @@
 
+<html>
 
 <head>
+
 <script type="text/javascript">
+document.write("<b>Welcome </b> \n to liujunyu s Page\n")
+
 function disp_alert()
 {
-alert("You are the best!");
+alert("have a good day！")
+
+
+
+
 }
 
-var express = require('express');
 
-var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Welcome to LIUjunyu s Page!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
 </script>
 </head>
 <body>
 
-<input type="button" onclick="disp_alert()" value="Who is the best"  />
+<input type="button" onclick="disp_alert()" value="click me" />
 
 </body>
+</html>
